047f25501918816fb6464ad6032f31a603ac3436
Anupama <anupama80286@gmail.com>
2022-01-18 00:41:41 +0530

    add tabView

--- TabViewExample/Views/ContentView.swift
+++ TabViewExample/Views/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tabIndex = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView (selection: $tabIndex){
+            Text("This tab's tag is \(tabIndex).")
+                .font(.headline)
+                .foregroundColor(.blue)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "tortoise")
+                        Text("Tab 1")
+                    }
+                }
+                .tag(1)
+            
+            Button("Take me to tab 3!") {
+                tabIndex = 3
+            }
+            .tabItem {
+                VStack {
+                    Image(systemName: "arrow.right.square")
+                    Text("Tab 2")
+                }
+            }
+            .tag(2)
+            List {
+                ForEach(0..<100) { _ in
+                    Text("This is tab 3!")
+                        .foregroundColor(.red)
+                }
+            }
+            .tabItem {
+                VStack {
+                    Image(systemName: "hands.clap")
+                    Text("Tab 3")
+                }
+            }
+            .tag(3)
+            
+        }
     }
 }
 

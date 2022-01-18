00457d09a062d5d3351796fb7925060d9708a14a
Anupama <anupama80286@gmail.com>
2022-01-18 22:51:44 +0530

    update contentView

--- GeometryReaderExample/GeometryReaderExample/ContentView.swift
+++ GeometryReaderExample/GeometryReaderExample/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 0 ){
+            GeometryReader{ geo in
+                Rectangle()
+                    .foregroundColor(.green)
+                    .onTapGesture {
+                        print("Width: \(geo .size.width), Height: \(geo.size.height)")
+                            
+                    }
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(width: geo.size.width*2/3, height: geo.size.height/4)
+                    .padding(.leading, geo.size.width/6)
+                    .padding(.top, geo.size.height/3)
+            }
+            GeometryReader{ geo in
+                Rectangle()
+                    .foregroundColor(.purple)
+                    .frame(width: geo.size.width/2)
+                
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(width: geo.size.width/2)
+                    .padding(.leading, geo.size.width/2)
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 

20f0a1d6b6cf8e3435d1e62e9076c566584387e5
Anupama <anupama80286@gmail.com>
2022-01-18 21:57:56 +0530

    initial commit- GeometryReaderExample

---
+++ GeometryReaderExample/GeometryReaderExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeometryReaderExample
+//
+//  Created by Anupama Biswas on 18/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeometryReaderExample/GeometryReaderExample/GeometryReaderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderExampleApp.swift
+//  GeometryReaderExample
+//
+//  Created by Anupama Biswas on 18/01/22.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

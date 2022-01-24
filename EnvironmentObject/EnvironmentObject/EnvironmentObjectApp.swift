6d1a33a414d9a294bd850f3736a9f4d80196b257
Anupama <anupama80286@gmail.com>
2022-01-24 18:28:16 +0530

    Initial Commit

---
+++ EnvironmentObject/EnvironmentObject/EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectApp.swift
+//  EnvironmentObject
+//
+//  Created by Anupama Biswas on 24/01/22.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

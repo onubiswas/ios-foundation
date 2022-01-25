095f730b63b2b18d85ddb9766a0f032c6fff4798
Anupama <anupama80286@gmail.com>
2022-01-26 00:15:07 +0530

    Initial Commit

---
+++ SwipeCard/SwipeCard/SwipeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeCardApp.swift
+//  SwipeCard
+//
+//  Created by Anupama Biswas on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwipeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

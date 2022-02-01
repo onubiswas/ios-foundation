059ee6de9cc140783fc8c6050bada68ea5ae1715
Anupama <anupama80286@gmail.com>
2022-02-01 23:50:19 +0530

    Initial Commit

---
+++ lazyStack_ScrollViewExample/lazyStack_ScrollViewExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lazyStack_ScrollViewExample
+//
+//  Created by Anupama Biswas on 01/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lazyStack_ScrollViewExample/lazyStack_ScrollViewExample/lazyStack_ScrollViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  lazyStack_ScrollViewExampleApp.swift
+//  lazyStack_ScrollViewExample
+//
+//  Created by Anupama Biswas on 01/02/22.
+//
+
+import SwiftUI
+
+@main
+struct lazyStack_ScrollViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

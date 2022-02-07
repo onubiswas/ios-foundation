cdfda52cc05e7298996bfabc4dea39ef1ba20a18
Anupama <anupama80286@gmail.com>
2022-02-07 21:53:02 +0530

    Picker with two types of pickerStyle modifier

--- SwiftUIPickerExample/SwiftUIPickerExample/ContentView.swift
+++ SwiftUIPickerExample/SwiftUIPickerExample/ContentView.swift
@@ -8,10 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    
+    @State var selectedIndex = 1
+        
+        var body: some View {
+            
+            VStack {
+            
+                Picker("Tap Me", selection: $selectedIndex) {
+                    Text("Option 1").tag(1)
+                    Text("Option 2").tag(2)
+                    Text("Option 3").tag(3)
+                }
+                .pickerStyle(MenuPickerStyle())
+                //  .pickerStyle(SegmentedPickerStyle())
+                 
+                Text("You've selected: \(selectedIndex)")
+            }
+            
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {

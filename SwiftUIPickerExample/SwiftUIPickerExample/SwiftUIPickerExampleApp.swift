320abfc0aca8d6a587c875503eb98a8f1e92863b
Anupama <anupama80286@gmail.com>
2022-02-07 21:10:14 +0530

    Initial Commit

---
+++ SwiftUIPickerExample/SwiftUIPickerExample/SwiftUIPickerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPickerExampleApp.swift
+//  SwiftUIPickerExample
+//
+//  Created by Anupama Biswas on 07/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPickerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
